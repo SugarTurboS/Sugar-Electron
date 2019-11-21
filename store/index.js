d8f539fb14e287883ca093e7c0732db25de96dff
唐正 <tangzheng@cvte.com>
2019-11-21 16:48:23 +0800

    插件模块问题修复

---
+++ store/index.js
@@ -0,0 +1,7 @@
+const { remote } = require('electron');
+const isRenderProcess = !!remote; // 是否是渲染进程
+if (isRenderProcess) {
+    module.exports = require('./render');
+} else {
+    module.exports = require('./main');
+}
